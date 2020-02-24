79dc45939a632e727e1eec543f42997aa58e51e2
Zach <zjsievers@gmail.com>
2020-02-24 10:43:14 -0500

    keypress to underscore doc / G.R , letters guess on console

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -2,25 +2,27 @@
 var word = ["skywalker", "stormtrooper", "jedi", "palpatine", "vader",
 ];
 var wins = 0; // start the game with 0 wins
-// var wordIndex = 0; // index to hold hangman words
+// var letterIndex = 0; // index to hold hangman words
 var guessesLeft = 10; // start game with 10 guess remaining
 var lettersGuessed; // letters guessed by user
 // var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 // 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
 // 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var randWord = Math.floor(Math.random() * word.length); 
-var chosenWord = word[randWord];
+// var randWord = Math.floor(Math.random() * word.length); 
+// var chosenWord = word[randWord];
 var underScore = [];
-// var rightWord = [];
-// var wrongWord = [];
+var win = document.getElementsByClassName("Wins");
 var docUnderScore = document.getElementsByClassName("underscore");
-
+var gameFinished = false;
 // Starts game
 
 
 onload= function() { // allows page to load before running script
 
-console.log(chosenWord); 
+    var randWord = Math.floor(Math.random() * word.length); 
+    var chosenWord = word[randWord];
+    var userGuess = event.key;
+console.log(chosenWord); // allows you to see the word in console
 
 // Press any letter to start the game
 document.addEventListener("keypress", function() {
@@ -38,61 +40,57 @@ var generateUnderscore = () => {
 }
 generateUnderscore (); // loop now = generateUnderscore
 
+// ----------------------------------------------------------------------------
 
 // get users guess
     document.onkeyup = function (event) { 
         var userGuess = event.key; 
 
-
-
-
-
 // empty array for letters guessed
     var lettersGuessed =[];
 
-
 // next step: add all user guesses into an array ; create a check loop
-        for (var i = 0; i < chosenWord.length; i++) {
+    for (var i = 0; i < chosenWord.length; i++) {
+
             if (chosenWord[i] === userGuess) {
                 lettersGuessed.push(i);
-                console.log("LetterExits");
-                console.log(lettersGuessed)
-            }
-        }    
-            
-            // check if guess is right
-            if (lettersGuessed.length <= 0) {
-                guessesLeft--;
-                console.log(guessesLeft)
+                // console.log("LetterExits");
+                // console.log(lettersGuessed)
             }
 
-            else {
-                // letters replace the "_"
-                for (var i = 0; i < lettersGuessed.length; i++) {
-                  underScore[lettersGuessed[i]] = userGuess;
-                  console.log(underScore[lettersGuessed[i]]);
-                  console.log([lettersGuessed[i]]);
+        } 
         
-        }
-        
-//update display to show letter instead of "_"
+        for (var i = 0; i < lettersGuessed.length; i++) {
+            underScore[lettersGuessed[i]] = userGuess;
+          //   console.log(underScore[lettersGuessed[i]]);
+          //   console.log([lettersGuessed[i]]);           
+      };    
+    
+      if (lettersGuessed.length <= 0) {
+        guessesLeft--;
+        console.log(guessesLeft)
+    }
 
-    updateDisplay();
-};
+
+                
+                updateDisplay() 
 };   
 
 
-// update display during the game
+// update display during the game ; brings letters to page
     function updateDisplay() {
         document.querySelector(".underscore").textContent = "";
         for (i = 0; i < underScore.length; i++) {
             document.querySelector(".underscore").textContent += 
             underScore[i];
         }
-        console.log(underScore)
-    };
 
-
+    }
+    
+    if ((userGuess === "chosenWord.length")) {
+        winText.textContent = "Wins: " + winText;
+    }
+     
 
 
 };
