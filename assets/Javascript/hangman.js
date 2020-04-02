4576f07f0e5df51287b459d48976ea86e3da3030
Zach <zjsievers@gmail.com>
2020-04-02 13:11:23 -0400

    variables cleaner // scoping issues

--- assets/Javascript/hangman.js
+++ assets/Javascript/hangman.js
@@ -14,7 +14,6 @@ var guessesRemaining = 10;
 // =========================================================================
 function startGame() {
     chosenWord = words[Math.floor(Math.random() * words.length)]; // randomizes words array
-
     lettersInWord = chosenWord.split("");// break word apart in indiv letters
     underscores = lettersInWord.length;
 
@@ -30,15 +29,68 @@ function startGame() {
 
     // display to html
     document.getElementById("currentWord").innerHTML = scoresAndLetters.join(" ");
+    document.getElementById("guessesLeft").innerHTML = guessesRemaining;
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("loss").innerHTML = loss;
 
     //testing
     console.log(chosenWord);
     console.log(lettersInWord);
     console.log(underscores);
     console.log(scoresAndLetters);
-  
+} 
 
+    // check if letter is in word
+function checkLetters(letter){
+    var keyInWord = false;
+
+    for (var i = 0; i < underscores; i++) {
+        if(chosenWord[i] == letter) {
+            keyInWord = true;
+        }
+    }
+
+    if (keyInWord) {
+        for (var i = 0; i < underscores; i++) {
+            if (chosenWord[i] == letter) {
+                scoresAndLetters[i] = letter;
+            }
+        }
+    }    
+    else {
+        wrongLetters.push(letter);
+        guessesRemaining --;
+    }
+
+    console.log(scoresAndLetters);
 }
-// main proces
+        
+
+
+function gameEnd() {
+    console.log("Wins: " + wins + " | Losses: " + loss + " | Guesses Remaining: " + guessesRemaining);
+    // user won
+    if (lettersInWord.toString() == scoresAndLetters.toString()) {
+        wins++;
+        alert("You won");
+
+        // display win
+        document.getElementById("wins").innerHTML = wins;
+
+        startGame();
+    }
+    // user lost
+}
+// main process
 // =========================================================================
+// generates code for first time
 startGame();
+
+// keyclicks
+document.onkeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+    checkLetters(userGuess);
+    gameEnd();
+    console.log(userGuess)
+}
